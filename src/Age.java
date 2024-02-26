be370b36c35347bdc534a356af3a6f37a4fbbddb
Jabed <jabedhossain1200@gmail.com>
2024-02-26 15:46:51 +0600

    commit_1.1

--- src/Age.java
+++ src/Age.java
@@ -2,13 +2,29 @@ public class Age {
     public static void main(String[] args) {
         
         int anik = 23;
-        int toton = 24;
+        int toton = 40;
+        
         getDaysFromAge(anik);
         getDaysFromAge(toton);
     }
     public static void getDaysFromAge(int age){
-        System.out.print("User age is: ");
-        System.out.println(age+365);
+        System.out.print("User age in days: ");
+        System.out.println(age*365);
     }
+
+    // public static void main(String[] args){
+    //     Function f = new Function();
+    //     f.sayHello();
+
+    //     int anik = 23;
+    //     int toton = 45;
+    //     Age countAge = new Age();
+    //     countAge.getDaysFromAge(anik);
+    //     countAge.getDaysFromAge(toton);
+    // }
+    // public void getDaysFromAge(int age){
+    //     System.out.print("User age: ");
+    //     System.out.println(age*365);
+    // }
     
 }
